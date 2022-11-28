589e14026072a48d07b0e99fcb2f32c2cdfce40b
Svyatoslav Kryukov <s.g.kryukov@yandex.ru>
2022-11-28 22:53:36 +0300

    Fix require mode

--- lib/rubocop/gradual.rb
+++ lib/rubocop/gradual.rb
@@ -11,8 +11,3 @@ module RuboCop
     class Error < StandardError; end
   end
 end
-
-if ENV["NO_GRADUAL"] != "1" && (RuboCop::ConfigLoader.loaded_features & %w[rubocop-gradual rubocop/gradual]).any?
-  require_relative "gradual/patch"
-  RuboCop::CLI.prepend(RuboCop::Gradual::Patch)
-end
